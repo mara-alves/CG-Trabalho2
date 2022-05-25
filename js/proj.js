06c93448bea1be2dd79e5a272d40158d3ba51b2a
AbelhaMaia16 <ffm1604@gmail.com>
2022-05-25 09:07:21 +0100

    Added base function declarations

---
+++ js/proj.js
@@ -0,0 +1,35 @@
+/*global THREE*/
+
+var camera, material, geometry;
+
+function createCameras() {
+
+}
+
+function createScene() {
+
+}
+
+function init() {
+
+}
+
+function animate() {
+
+}
+
+function render() {
+
+}
+
+function onResize() {
+
+}
+
+function onKeyUp(e) {
+
+}
+
+function onKeyDown(e) {
+
+}
